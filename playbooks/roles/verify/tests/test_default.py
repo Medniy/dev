2587bc2a1ff3bd116a41d9d0a4cc5176eb4225fc
Medniy <prokopenkoae85@yandex.ru>
2024-11-07 07:21:20 +0000

    18p1

---
+++ playbooks/roles/verify/tests/test_default.py
@@ -0,0 +1,15 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
+
+def test_sshd_is_installed(host):
+    sshd = host.package("openssh-server")
+    assert sshd.is_installed
+
+def test_sshd_running_and_enabled(host):
+    sshd = host.service("sshd")
+    assert sshd.is_running
+    assert sshd.is_enabled
